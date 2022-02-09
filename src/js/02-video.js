1a00e71a9ed25bb41d453bbd64662c978034f6fa
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-02-10 01:29:07 +0200

    task 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,50 @@
 
+// <!-- Добавь библиотеку как зависимость проекта через npm.
+// Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+// Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
+// Сохраняй время воспроизведения в локальное хранилище.
+// Пусть ключом для хранилища будет строка "videoplayer-current-time".
+// При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
+// Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
+//  -->
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
+const onPlayTime = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+console.log(onPlayTime);
+player.on('timeupdate', throttle(onPlayTime, 1000)); 
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(savedTime);
+if (parsedTime.seconds === 571.563) {
+    localStorage.removeItem('videoplayer-current-time'); 
+  player
+    .setCurrentTime(parsedTime.seconds)
+    .then(function (seconds) {
+      console.log('seconds = the actual time that the player seeked to');
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          console.log('RangeError');
+          break;
+        default:
+          break;
+      }
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+// Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. 
+// Пусть ключом для хранилища будет строка "feedback-form-state".
+// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
+// При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
+// Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
+
+import throttle from 'lodash.throttle';
+
+const inputEmailEl = document.querySelector('input');
+const inputMessageEl = document.querySelector('textarea');
+const formEl = document.querySelector('form');
+
+
+formEl.addEventListener('input', throttle(inLocalStoradge, 500));
+formEl.addEventListener('submit', onSubmitForm);
+
+
+const savedData = localStorage.getItem('feedback-form-state');
+const parsedData = JSON.parse(savedData);
+
+if (parsedData) {
+  inputEmailEl.value = parsedData.email;
+  inputMessageEl.value = parsedData.message;
+}
+
+console.log(localStorage);
+
+function inLocalStoradge() {
+    const email = inputEmailEl.value;
+    const message = inputMessageEl.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
+}
+
+function onSubmitForm(event) {
+    event.preventDefault();
+    const email = event.currentTarget.elements.email.value;
+    const message = event.currentTarget.elements.message.value;
+    
+    if (email === "" || message === "") {
+    return alert('Пожалуйста, заполните все поля');
+}
+
+    const objectData = {
+    message,
+    email,
+    };
+    console.log(objectData);
+
+    localStorage.removeItem('feedback-form-state');
+    formElem.reset();
+}
+
+
 
